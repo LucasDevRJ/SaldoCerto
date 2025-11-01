e3bcc6d3838a3d4a32209bfafa8914c158118553
Lucas <lucaspereiradelima2020@gmail.com>
2025-11-01 16:44:07 -0300

    Método para criar conta.

--- src/conta/Conta.java
+++ src/conta/Conta.java
@@ -1,8 +1,32 @@
 package conta;
 
+import java.util.Scanner;
+
 public class Conta {
 
     private String titular;
     private Tipo tipo;
     private double saldo;
+
+    public String getTitular() {
+        return this.titular;
+    }
+
+    private void setTitular(String titular) {
+        if (titular == null || titular.isBlank()) {
+            System.out.println("O nome do títular é nulo ou vázio.");
+        } else {
+            this.titular = titular;
+        }
+    }
+
+    public Conta criarConta() {
+        Conta conta = new Conta();
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o seu nome: ");
+        conta.setTitular(entrada.nextLine());
+
+        return conta;
+    }
 }

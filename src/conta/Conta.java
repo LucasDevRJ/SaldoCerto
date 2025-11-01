a088316d27972b19d99f2b00fe0d75c9250598ba
Lucas <lucaspereiradelima2020@gmail.com>
2025-11-01 17:35:49 -0300

    Refatorações na classe Conta.

--- src/conta/Conta.java
+++ src/conta/Conta.java
@@ -8,20 +8,12 @@ public class Conta {
     private Tipo tipo;
     private double saldo;
 
-    public String getTitular() {
-        return this.titular;
-    }
-
     private void setTitular(String titular) {
         if (titular == null || titular.isBlank()) {
             System.out.println("O nome do títular é nulo ou vázio.");
         } else {
             this.titular = titular;
         }
-    }
-
-    public Tipo getTipo() {
-        return this.tipo;
     }
 
     private void setTipo(int tipoContaDesejada) {
@@ -37,10 +29,6 @@ public class Conta {
         }
     }
 
-    public double getSaldo() {
-        return this.saldo;
-    }
-
     private void setSaldo(double saldo) {
         if (saldo <= 0) {
             System.out.println("Saldo inválido.");
@@ -49,7 +37,7 @@ public class Conta {
         }
     }
 
-    public Conta criarConta() {
+    public void criarConta() {
         Conta conta = new Conta();
         Scanner entrada = new Scanner(System.in);
 
@@ -63,8 +51,6 @@ public class Conta {
 
         System.out.print("Digite seu saldo inicial: R$ ");
         conta.setSaldo(entrada.nextDouble());
-
-        return conta;
     }
 
     public String exibeInformacoesDaConta() {
@@ -76,5 +62,17 @@ public class Conta {
                 --------------------------------------------------------------
                 """.formatted(this.titular, this.tipo, this.saldo);
         return informacoes;
+    }
+
+    public void consultarSaldo() {
+        System.out.println("--------------------|Seu Saldo Atual|--------------------");
+        System.out.println(String.format("O saldo atual é R$ %.2f", this.saldo));
+        System.out.println("---------------------------------------------------------");
+    }
+
+    public void receberValor(double valor) {
+        double novoSaldo = this.saldo + valor;
+        setSaldo(novoSaldo);
+        System.out.println("Saldo atualizado R$ " + this.saldo);
     }
 }

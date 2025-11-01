cae58513a63b5b3ac75d6a26306512bbcf5deb64
Lucas <lucaspereiradelima2020@gmail.com>
2025-11-01 16:48:58 -0300

    Método para escolher tipo de conta.

--- src/conta/Conta.java
+++ src/conta/Conta.java
@@ -20,12 +20,34 @@ public class Conta {
         }
     }
 
+    public Tipo getTipo() {
+        return this.tipo;
+    }
+
+    private void setTipo(int tipoContaDesejada) {
+        switch (tipoContaDesejada) {
+            case 1:
+                this.tipo = Tipo.CORRENTE;
+                break;
+            case 2:
+                this.tipo = Tipo.POUPANCA;
+                break;
+            default:
+                System.out.println("Opção inválida.");
+        }
+    }
+
     public Conta criarConta() {
         Conta conta = new Conta();
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("Digite o seu nome: ");
         conta.setTitular(entrada.nextLine());
+
+        System.out.println("1 - Conta Corrente");
+        System.out.println("2 - Conta Poupança");
+        System.out.print("Selecione o tipo de conta: ");
+        conta.setTipo(entrada.nextInt());
 
         return conta;
     }
